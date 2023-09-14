6896569d2d1b58fa84e3163e63d9a2bdf5d12215
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2023-09-14 12:16:28 -0700

    initializing shopping app with firebase

--- App.js
+++ App.js
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+//import react navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const App = () => {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyDi-xncyFZuo_WUl0Y-KxW5x6lfN6Nlq7A',
+    authDomain: 'shopping-list-demo-5dc5e.firebaseapp.com',
+    projectId: 'shopping-list-demo-5dc5e',
+    storageBucket: 'shopping-list-demo-5dc5e.appspot.com',
+    messagingSenderId: '937853527001',
+    appId: '1:937853527001:web:6671dd471ce18d7384afc8',
+  };
+
+  //initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  //Initialize cloud firestore and get a reference to the sevice
+  const db = getFirestore(app);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator></Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
